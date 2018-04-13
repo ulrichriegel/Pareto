317ecbe35d9067a82fc4d5718c0acee0192c5c09
ulrichriegel <ulrich.riegel@gmx.de>
2018-04-14 00:20:44 +0200

    Added example data to the package.

---
+++ R/Documentation.R
@@ -0,0 +1,14 @@
+#' Example data: Atachment Points
+#'
+"Example1_AP"
+
+#' Example data: Expected Losses
+#'
+#'
+"Example1_EL"
+
+
+#### ' @useDynLib glrm
+#### ' @importFrom Rcpp sourceCpp
+# NULL
+
